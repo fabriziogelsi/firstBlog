ee4d1a5c4696e6d9a56477f284bca1a006e08c5a
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2020-08-09 12:14:37 -0300

    add database setup migration

---
+++ BEComentarios/BEComentarios/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BEComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEComentarios.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ComentarioDataModel> Comentario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=ABMComentarios;Uid=root;Pwd=Reja2020.-");
+        }
+    }
+}
--- BEComentarios/BEComentarios/Models/ComentarioDataModel.cs
+++ BEComentarios/BEComentarios/Models/ComentarioDataModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BEComentarios.Models
 {
-    public class Comentario
+    public class ComentarioDataModel
     {
         [Required]
         public int Id { get; set; }
