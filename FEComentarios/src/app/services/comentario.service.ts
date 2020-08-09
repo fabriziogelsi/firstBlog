38f1eeff4f780b435517ba0e3023ec0c1c9a2aa6
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2020-08-09 14:07:52 -0300

    Service FE implemented

--- FEComentarios/src/app/services/comentario.service.ts
+++ FEComentarios/src/app/services/comentario.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { comentario } from '../models/comentario';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +9,34 @@ import { HttpClient } from '@angular/common/http'
 export class ComentarioService {
 
   myApiUrl = 'https://localhost:44374/';
-  myApiController = 'api/Comentario';
+  myApiController = 'api/Comentario/';
 
   httpOptions = {
-    headers: new Headers({
+    headers: new HttpHeaders({
       'Content-type': 'application/json'
     })
   };
 
   constructor(private http: HttpClient) { }
+
+  getListComentarios(): Observable<comentario[]> {
+    return this.http.get<comentario[]>(this.myApiUrl + this.myApiController);
+  }
+
+  deleteComentario(id: number): Observable<comentario> {
+    return this.http.delete<comentario>(this.myApiUrl + this.myApiController + id);
+  }
+
+  guardarComentario(comentario: comentario): Observable<comentario> {
+    return this.http.post<comentario>(this.myApiUrl + this.myApiController, comentario, this.httpOptions);
+  }
+
+  cargarComentario(id: number): Observable<comentario> {
+    return this.http.get<comentario>(this.myApiUrl + this.myApiController + id);
+  }
+
+  actualizarComentario(id: number, comentario: comentario): Observable<comentario> {
+    return this.http.put<comentario>(this.myApiUrl + this.myApiController + id, comentario, this.httpOptions);
+  }
+
 }
