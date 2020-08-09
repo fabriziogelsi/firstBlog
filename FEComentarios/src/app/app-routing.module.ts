242bc0e827f64df2e10c3b20564246007dd1db62
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2020-08-08 23:24:38 -0300

    routing added

--- FEComentarios/src/app/app-routing.module.ts
+++ FEComentarios/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
+import { VerComentariosComponent } from './components/ver-comentarios/ver-comentarios.component';
+import { ListComentarionComponent } from './components/list-comentarion/list-comentarion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'agregar', component: AgregarEditarComentarioComponent},
+  { path: 'editar/:id', component: AgregarEditarComentarioComponent},
+  { path: 'ver/:id', component: VerComentariosComponent},
+  { path: '', component: ListComentarionComponent, pathMatch: 'full'},
+  { path: '**', redirectTo: '/'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
