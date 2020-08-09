bdd5183016ab392f0fd1451026386b8b795893ed
Fabrizio Gelsi <fabrizio.gelsi@gmail.com>
2020-08-09 14:43:48 -0300

    list and view comments using service with DB connection data

--- FEComentarios/src/app/components/list-comentarion/list-comentarion.component.ts
+++ FEComentarios/src/app/components/list-comentarion/list-comentarion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { comentario } from '../../models/comentario';
+import { ComentarioService } from '../../services/comentario.service';
 
 @Component({
   selector: 'app-list-comentarion',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComentarionComponent implements OnInit {
 
-  constructor() { }
+  listComentarios: comentario[];
+  loading = false;
+
+  constructor(private comentarioService: ComentarioService) { }
 
   ngOnInit(): void {
+    this.cargarComentario();
+  }
+
+  cargarComentario(){
+    this.loading = true;
+    this.comentarioService.getListComentarios().subscribe(data => {
+        this.loading = false;
+        this.listComentarios = data;
+    })
+  }
+
+  delete(id: number){
+    this.loading = true;
+    this.comentarioService.deleteComentario(id).subscribe(data => {
+      this.loading = false;
+      this.cargarComentario();
+    })
   }
 
 }
--- FEComentarios/src/app/components/ver-comentarios/ver-comentarios.component.ts
+++ FEComentarios/src/app/components/ver-comentarios/ver-comentarios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { comentario } from '../../models/comentario';
+import { ComentarioService } from 'src/app/services/comentario.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ver-comentarios',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerComentariosComponent implements OnInit {
 
-  constructor() { }
+  comentario: comentario;
+  loading = false;
+  idComentario: number;
+
+  constructor(private comentarioService: ComentarioService, private route: ActivatedRoute) { 
+    this.idComentario = +this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit(): void {
+    this.cargarComentario();
+  }
+
+  cargarComentario(){
+    this.loading = true;
+    this.comentarioService.cargarComentario(this.idComentario).subscribe(data => {
+      this.loading = false;
+      this.comentario = data;
+    })
   }
 
 }
